52d19b1983c01620d25106b3ec0a66b5b3130acf
AnthonySorise <contact@anthonysorise.com>
2017-10-23 19:56:15 -0700

    API Prototypes

--- youtube_api/youtubeapi.js
+++ youtube_api/youtubeapi.js
@@ -2,24 +2,57 @@
 $(document).ready(clickHandlers);
 
 function clickHandlers(){
-    $('button').click(searchYoutube)
+    $('.channelButton').click(searchChannels)
+    $('.channelVideosButton').click(searchVideosWithinChannel)
 }
 
 // function getTextString(){
 //     return $('input').text();
 // }
 
-
-function searchYoutube(string) {
-    string = $('input').val();
+//Channel Search
+function searchChannels() {
+    string = $('.inputSearchChannel').val();
     $.ajax({
         url: 'https://www.googleapis.com/youtube/v3/search',
         dataType: 'json',
         method: 'get',
         data: {
-            key: 'AIzaSyAOr3VvEDRdI5u9KGTrsJ7usMsG5FWcl6s',
+            key: "AIzaSyAOr3VvEDRdI5u9KGTrsJ7usMsG5FWcl6s",
             q: string,
-            type: 'channelSection',
+            type: 'channel',
+            part: 'snippet',
+            maxResults: 10
+        },
+        success: function (data) {
+            console.log('Youtube success',data);
+            for(var i=0; i<data.items.length; i++){
+                let responseDiv = $("<div>");
+                $('<iframe>',{
+                    src: 'https://www.youtube.com/embed/'+data.items[i].id.videoId,
+                }).appendTo(responseDiv);
+                $(".response").append(responseDiv);
+            }
+
+        },
+        error: function (data) {
+            console.log('something went wrong with YT', data);
+        }
+    })
+}
+
+//Video Search Within Channel
+function searchVideosWithinChannel() {        //string = channel ID
+    string = $('.inputSearchChannelVideos').val();
+    $.ajax({
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        dataType: 'json',
+        method: 'get',
+        data: {
+            key: "AIzaSyAOr3VvEDRdI5u9KGTrsJ7usMsG5FWcl6s",
+            channelId:string,
+            type: 'video',
+            order:'date',
             part: 'snippet',
             maxResults: 10
         },
