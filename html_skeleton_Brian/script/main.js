78c3da323d6dc108c4ca15180f1aa408ae1f87fa
briandhkim <w1057216@gmail.com>
2017-10-25 15:57:50 -0700

    some progress over stats

--- html_skeleton_Brian/script/main.js
+++ html_skeleton_Brian/script/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-	// $('#topFiller').css('height',$('nav').height());
+	
+	/**
+		function for preventing page refresh with search button; 
+		only did it because page refresh was annoying
+	 **/
 	$('#mainNav-option form button, #midNav-option form button').click(function(event){
 		event.preventDefault();
 	});
+
+	/*** button target for opening theater mode ***/
 	$('.lightBoxMode').click(function(){
 		$('#lightBoxModal').modal('show');
 	});
+	/*** ***/
+	$('.lightBoxMode').tooltip('hover focus');
+	// $('.videoStats').click(function(){
+	// 	$('.videoStats').popover('toggle');
+	// });
+	$('#videoStats').popover('hover focus');
 });
