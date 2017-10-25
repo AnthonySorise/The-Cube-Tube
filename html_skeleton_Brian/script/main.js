1bd9b64e9def03fd3e238bb582ae405fe300b74e
briandhkim <w1057216@gmail.com>
2017-10-25 15:16:37 -0700

    lightbox started

--- html_skeleton_Brian/script/main.js
+++ html_skeleton_Brian/script/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
 	// $('#topFiller').css('height',$('nav').height());
+	$('#mainNav-option form button, #midNav-option form button').click(function(event){
+		event.preventDefault();
+	});
+	$('.lightBoxMode').click(function(){
+		$('#lightBoxModal').modal('show');
+	});
 });
