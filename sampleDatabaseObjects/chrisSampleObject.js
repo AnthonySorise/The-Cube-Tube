1eac90d5e4af34b7261b2a7138125bb529fbc4e0
ckim618 <ckim618@gmail.com>
2017-10-26 17:39:02 -0700

    created dummy data for video info into object

---
+++ sampleDatabaseObjects/chrisSampleObject.js
@@ -0,0 +1,167 @@
+sampleData = {
+
+    "bRuLGVagk84": {
+        snippet: {
+            description: "College Kids try Halloween Candy! Watch all People Vs Food Eps! http://goo.gl/KjLw5C SUBSCRIBE THEN HIT THE ! New Videos 12pm PT on REACT!",
+            publishedAt: "2017-10-26T19:00:02.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/bRuLGVagk84/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/bRuLGVagk84/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/bRuLGVagk84/mqdefault.jpg"
+                },
+                standard: {
+                    url: ""
+                }
+            },
+            title: "GUESS THAT HALLOWEEN CANDY CHALLENGE! | COLLEGE KIDS vs. FOOD"
+        },
+        statistics: {
+            dislikeCount:"110",
+            favoriteCount:"0",
+            likeCount:"6141",
+            viewCount:"70136"
+        }
+    },
+    "imfhW8sk2mg": {
+        snippet: {
+            description: "We discuss the breaking news that Disney have decided to SCRAP the Eastern Gateway Project in favour of a new Western Gateway into Downtown Disney",
+            publishedAt: "2017-10-26T01:26:47.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/imfhW8sk2mg/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/imfhW8sk2mg/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/imfhW8sk2mg/mqdefault.jpg"
+                },
+                standard: {
+                    url: ""
+                }
+            },
+            title: "Disney SCRAPS Eastern Gateway for Downtown Disney Entrance Area - Disney News - 10/25/17"
+        },
+        statistics: {
+            dislikeCount:"8",
+            favoriteCount:"0",
+            likeCount:"1009",
+            viewCount:"20242"
+        }
+    },
+    "DKVJ52qwWWQ": {
+        snippet: {
+            description: "Full playthrough of Castlevania: Chorus of Mysteries (NES Hack) by Mike Matei↵Twitter Mike ✜ https://twitter.com/Mike_Matei↵↵This is edited down from two live streams that were done on the Cinemassacre Plays Channel. If you'd like to see the fully unedited playthroughs use these links↵Part 1 https://www.youtube.com/watch?v=ub8zPpui0p0↵Part 2 https://www.youtube.com/watch?v=NGg1fyJJgmQ",
+            publishedAt: "2017-10-24T05:42:14.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/DKVJ52qwWWQ/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/DKVJ52qwWWQ/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/DKVJ52qwWWQ/maxresdefault.jpg"
+                },
+                standard: {
+                    url: "https://i.ytimg.com/vi/DKVJ52qwWWQ/sddefault.jpg"
+                }
+            },
+            title: "Castlevania: Chorus of Mysteries - Full Playthrough"
+        },
+        statistics: {
+            dislikeCount:"305",
+            favoriteCount:"0",
+            likeCount:"1758",
+            viewCount:"101358"
+        }
+    },
+    "wg6PfxpPQeM": {
+        snippet: {
+            description: "Get the whole family together for breakfast!↵↵Check out more awesome BuzzFeedFood videos! ↵http://bit.ly/YTbuzzfeedfood↵↵↵MUSIC↵All Heart↵Licensed via Warner Chappell Production Music Inc. ↵↵SFX provided by Audioblocks. ↵(https://www.Audioblocks.com)↵↵Footage provided by VideoBlocks↵http://vblocks.co/x/BuzzFeedYouTube↵↵↵Made by BFMP www.buzzfeed.com/videoteam↵↵↵GET MORE BUZZFEED:↵www.buzzfeed.com↵www.buzzfeed.com/video↵www.buzzfeed.com/videoteam↵www.youtube.com/buzzfeedvideo↵www.youtube.com/buzzfeedyellow↵www.youtube.com/buzzfeedblue↵www.youtube.com/buzzfeedviolet↵www.youtube.com/buzzfeed",
+            publishedAt: "2016-01-07T23:00:01.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/wg6PfxpPQeM/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/wg6PfxpPQeM/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/wg6PfxpPQeM/mqdefault.jpg"
+                },
+                standard: {
+                    url: "https://i.ytimg.com/vi/wg6PfxpPQeM/sddefault.jpg"
+                }
+            },
+            title: "2 Breakfast Bake Recipes"
+        },
+        statistics: {
+            dislikeCount:"85",
+            favoriteCount:"0",
+            likeCount:"9090",
+            viewCount:"555143"
+        }
+    },
+    "xNddRhpx5tA": {
+        snippet: {
+            description: "For October, we like going back and hitting some older horror films we've never covered. And Friday the 13th Part 2 has oodles of sins. ↵↵Next week: A couple more creepy sins.↵↵Remember, no movie is without sin! Which movie's sins should we expose next?!↵↵Podcast: http://soundcloud.com/cinemasins↵Sins Video Playlist: http://www.youtube.com/watch?v=wy-v4c4is-w&list=PLMWfZxj1nTkQBy4AeRGG4xH5d2IIApNPj↵Tweet us: http://twitter.com/cinemasins↵Reddit with us: http://reddit.com/r/cinemasins↵Tumble us: http://cinema-sins.tumblr.com↵Call us: 405-459-7466↵Jeremy's book: http://theablesbook.com",
+            publishedAt: "2017-10-12T16:06:03.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/xNddRhpx5tA/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/xNddRhpx5tA/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/xNddRhpx5tA/mqdefault.jpg"
+                },
+                standard: {
+                    url: "https://i.ytimg.com/vi/xNddRhpx5tA/sddefault.jpg"
+                }
+            },
+            title: "Everything Wrong With Friday the 13th Part 2"
+        },
+        statistics: {
+            dislikeCount:"488",
+            favoriteCount:"0",
+            likeCount:"24876",
+            viewCount:"1094813"
+        }
+    },
+    "gHgBk2fLLHY": {
+        snippet: {
+            description: "See how director Rian Johnson is taking the characters of a galaxy far, far away into new directions in this behind-the-scenes look at Star Wars: The Last Jedi.↵↵Visit Star Wars at http://www.starwars.com↵Subscribe to Star Wars on YouTube at http://www.youtube.com/starwars↵Like Star Wars on Facebook at http://www.facebook.com/starwars↵Follow Star Wars on Twitter at http://www.twitter.com/starwars↵Follow Star Wars on Instagram at http://www.instagram.com/starwars↵Follow Star Wars on Tumblr at http://starwars.tumblr.com/",
+            publishedAt: "2017-10-26T04:00:01.000Z",
+            thumbnails: {
+                default: {
+                    url: "https://i.ytimg.com/vi/gHgBk2fLLHY/default.jpg"
+                },
+                high: {
+                    url: "https://i.ytimg.com/vi/gHgBk2fLLHY/hqdefault.jpg"
+                },
+                medium: {
+                    url: "https://i.ytimg.com/vi/gHgBk2fLLHY/mqdefault.jpg"
+                },
+                standard: {
+                    url: "https://i.ytimg.com/vi/gHgBk2fLLHY/sddefault.jpg"
+                }
+            },
+            title: "Directing The Last Jedi"
+        },
+        statistics: {
+            dislikeCount:"156",
+            favoriteCount:"0",
+            likeCount:"15868",
+            viewCount:"218422"
+        }
+    }
+
+
+}
