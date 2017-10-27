b7382cd571bf405099b8ea07b402dcc90c4802bc
ckim618 <ckim618@gmail.com>
2017-10-26 18:41:38 -0700

    working on search button to console log retun object

--- html_skeleton_Brian/script/main.js
+++ html_skeleton_Brian/script/main.js
@@ -14,9 +14,38 @@ $(document).ready(function(){
 	});
 	/*** ***/
 	$('[data-toggle="tooltip"]').tooltip();	//needed for tooltip
-	$('[data-toggle="popover"]').popover();
+	// $('[data-toggle="popover"]').popover();
 	// $('.videoStats').click(function(){
 	// 	$('.videoStats').popover('toggle');
 	// });
 	// $('#videoStats').popover('hover focus');
+
+	//Click handler to console log search results
+	function clickHandler() {
+		console.log('Search button was clicked');
+		$(".btn-danger").on('click', searchChannelsByName);
+    }
+
+    //Channel Search by Name
+    function searchChannelsByName() {
+        string = $('#channelSearchInput').val();
+        $.ajax({
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            dataType: 'json',
+            method: 'get',
+            data: {
+                key: "AIzaSyAOr3VvEDRdI5u9KGTrsJ7usMsG5FWcl6s",
+                q: string,
+                type: 'channel',
+                part: 'snippet',
+                maxResults: 10
+            },
+            success: function (data) {
+                console.log('Youtube success',data);
+            },
+            error: function (data) {
+                console.log('something went wrong with YT', data);
+            }
+        })
+    }
 });
