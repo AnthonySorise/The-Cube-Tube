4f129ab88dbac0358ef70034b75f33a5560aaeaf
collin_wong <collinwong53@gmail.com>
2017-11-02 14:38:47 -0700

    finished read_videos_by_channel, need to test

--- script/api_calls_to_db/access_database/read_videos_by_channel.php
+++ script/api_calls_to_db/access_database/read_videos_by_channel.php
@@ -6,8 +6,10 @@ if(empty($offset)){
     $output['errors'][] = "MISSING OFFSET";
 }
 $user_id = $_POST['user_id'];
-$stmt = $conn->prepare();
-$stmt->bind_param("s",$search,$table);
+$stmt = $conn->prepare("SELECT `c.channel_title`, `c.youtube_channel_id`,`c.description`,`c.sub_count`,
+`c.video_count`,`c.view_count`,`c.thumbnail_file_name` FROM `channels` AS `c` JOIN `channels_to_users` AS `ctu`
+ON `c.channel_id` = `ctu.channel_id` WHERE `ctu.user_id` = ? ORDER BY `c.channel_title`");
+$stmt->bind_param("s",$user_id);
 $stmt->execute();
 if(!empty($stmt)){
     //if($result->num_rows!==0)
@@ -21,5 +23,5 @@ if(!empty($stmt)){
     }
 }
 ?>
-SELECT c.channel_title, c.youtube_channel_id,c.description,c.sub_count,
-c.video_count,c.view_count,c.thumbnail_file_name,
+
+
