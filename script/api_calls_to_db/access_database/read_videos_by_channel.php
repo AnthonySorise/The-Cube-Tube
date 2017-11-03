5c20cf53acdd90bf4bc6ff671a303e1aab2a66ed
collin_wong <collinwong53@gmail.com>
2017-11-03 16:37:34 -0700

    fixed read vid channel

--- script/api_calls_to_db/access_database/read_videos_by_channel.php
+++ script/api_calls_to_db/access_database/read_videos_by_channel.php
@@ -8,7 +8,7 @@ if(empty($channel_id)){
     $output['errors'][] = "MISSING USER ID";
 }
 $stmt = $conn->prepare("SELECT v.youtube_video_id,v.description,v.published_at 
-FROM videos JOIN channels AS c ON v.youtube_channel_id = c.youtube_channel_id
+FROM videos AS v JOIN channels AS c ON v.youtube_channel_id = c.youtube_channel_id
 WHERE v.youtube_channel_id = ? 
 ORDER BY v.published_at DESC LIMIT 40 OFFSET ?");
 $stmt->bind_param("si",$youtube_channel_id,$offset);
