9f2ac2ff4cebdf95d39065924fae82773f1e974c
AnthonySorise <contact@anthonysorise.com>
2017-11-03 19:14:47 -0700

    fix

--- script/main.js
+++ script/main.js
@@ -545,9 +545,9 @@ function manageDatabaseWithChannelId (channelID){
                 console.log('data.nothing_to_read', data.nothing_to_read)
                 if(data.nothing_to_read){
                     console.log("NOT ON DATABASE")
-                    convertYTApiVideoDatatoDbData(channelId);       //READ AND CHECK if exists on db FIRST!
-                    var ytChannelData = convertYTApiChannelDatatoDbData(channelId);
-                    access_database.insert_channel(channelId)
+                    convertYTApiVideoDatatoDbData(channelID);       //READ AND CHECK if exists on db FIRST!
+                    var ytChannelData = convertYTApiChannelDatatoDbData(channelID);
+                    access_database.insert_channel(channelID)
 
                     function handleGlobalVideoObjectArray() {
                         if (globalVideoObjectArray === null) {
