0bb7e909dcdc50d75a896c11d27d93892adeef32
collin_wong <collinwong53@gmail.com>
2017-11-04 13:29:04 -0700

    missed a comma

--- script/api_calls_to_db/access_database/insert_video.php
+++ script/api_calls_to_db/access_database/insert_video.php
@@ -3,7 +3,7 @@ if(empty($LOCAL_ACCESS)){
     die('direction access not allowed');
 }
 $video_array = $_POST['videoArray'];
-$channel_id = $video_array[$i]['channel_id'];
+$channel_id = $video_array['channel_id'];
 for($i = 0; $i<count($video_array); $i++ ){
     $video_title = $video_array[$i]['video_title'];
     $youtube_channel_id = $video_array[$i]['youtube_channel_id'];
