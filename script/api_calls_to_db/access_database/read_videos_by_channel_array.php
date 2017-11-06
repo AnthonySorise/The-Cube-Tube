36c5fd911c7f996e15283190674a008982b14fe4
collin_wong <collinwong53@gmail.com>
2017-11-06 15:34:54 -0800

    testing where in

--- script/api_calls_to_db/access_database/read_videos_by_channel_array.php
+++ script/api_calls_to_db/access_database/read_videos_by_channel_array.php
@@ -10,8 +10,6 @@ if(count($youtube_array)>1){
     }
     $channels = '('.$channels.')';
 };
-print($channels);
-exit();
 $offset = $_POST['offset'];
 if(empty($youtube_array)){
     $output['errors'][] = 'MISSING YOUTUBE ARRAY';
