1fffc19abddaec761c97c4c5760cba905b9a314e
collin_wong <collinwong53@gmail.com>
2017-11-06 15:53:52 -0800

    testing in

--- script/api_calls_to_db/access_database/read_videos_by_channel_array.php
+++ script/api_calls_to_db/access_database/read_videos_by_channel_array.php
@@ -8,7 +8,6 @@ if(count($youtube_array)>1){
     for($i=1; $i<count($youtube_array); $i++){
         $channels = $channels.','."'{$youtube_array[$i]}'";
     }
-    $channels = '('.$channels.')';
 };
 $offset = $_POST['offset'];
 if(empty($youtube_array)){
@@ -19,10 +18,15 @@ if(!isset($offset)){
 }
 $stmt = $conn->prepare("SELECT v.youtube_video_id,v.description,v.published_at, v.video_title, c.channel_title, c.youtube_channel_id
 FROM videos AS v JOIN channels AS c ON v.youtube_channel_id = c.youtube_channel_id
-WHERE c.youtube_channel_id IN ?
+WHERE c.youtube_channel_id IN (?)
 ORDER BY v.published_at DESC LIMIT 40 OFFSET ?");
+if(!$stmt){
+    echo "prepared failed:(".$conn->errno . ")".$conn->error;
+}
 $stmt->bind_param('si',$channels,$offset);
-$stmt->execute();
+if(!$stmt->execute()){
+    echo "Execute failed: (" . $stmt->errno . ")" . $stmt->error;
+};
 $result = mysqli_stmt_get_result($stmt);
 if(!empty($result)) {
     if (mysqli_num_rows($result) > 0) {
@@ -31,7 +35,7 @@ if(!empty($result)) {
             $output['data'][] = $row;
         }
     } else {
-        $output['errors'][] = mysqli_error($conn);
+        $output['errors'][] = 'no results';
     }
 }else{
     $output['errors'][] = 'INVALID QUERY';
