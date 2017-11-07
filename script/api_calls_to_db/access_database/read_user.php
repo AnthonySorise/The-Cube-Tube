a6419b4ce366b0607bc1ab9acd79e8491e50c5d3
collin_wong <collinwong53@gmail.com>
2017-11-06 19:50:19 -0800

    added read users

---
+++ script/api_calls_to_db/access_database/read_user.php
@@ -0,0 +1,26 @@
+<?php
+if(empty($LOCAL_ACCESS)){
+    die('direction access not allowed');
+}
+$output['data'] = [];
+$user_link = $_POST['user_link'];
+if(empty($user_link)){
+    $output['errors'][] ='MISSING USER_LINK';
+}
+$stmt = $conn->prepare("SELECT * FROM `users` WHERE `user_link`=?");
+$stmt->bind_param('s',$user_link);
+$stmt->execute();
+$results = mysqli_stmt_get_result($stmt);
+if(!empty($results)){
+    if(mysqli_num_rows($results)>0){
+        $output['success']=true;
+        while($row = mysqli_fetch_assoc($results)){
+            $output['data'][] = $row;
+        }
+    }else{
+        $output['errors'][] = 'NOTHING TO READ';
+    }
+}else{
+    $output['errors'][] = 'INVALID QUERY';
+}
+?>
