edc4f6167423fbb024144d1bbb3d92d7273c7715
ckim618 <ckim618@gmail.com>
2017-11-11 10:34:18 -0800

    mereging

--- script/main.js
+++ script/main.js
@@ -1133,15 +1133,5 @@ if(window.matchMedia("(min-width: 1020px)").matches) {
     $('#mainNav').append(completedSearchDiv);
 }
 
-$(window).bind('orientationchange resize', function(event){
-    if (event.orientation) {
-      if (event.orientation == 'landscape') {
-        if (window.rotation == 90) {
-          rotate(this, -90);
-        } else {
-          rotate(this, 90);
-        }
-      }
-    }
-  });
+
   
