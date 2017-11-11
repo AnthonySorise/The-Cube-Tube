5001c67870a0f036c4a9e760edb02df0dc8c3718
collin_wong <collinwong53@gmail.com>
2017-11-10 19:06:10 -0800

    trying to relocate

--- script/api_calls_to_db/access_database/insert_ctu.php
+++ script/api_calls_to_db/access_database/insert_ctu.php
@@ -2,7 +2,7 @@
 if(empty($LOCAL_ACCESS)){
     die('direct access not allowed');
 }
-nclude('./insert_user.php');
+include('./insert_user.php');
 //makes user link if session and get is empty
 if(!isset($_SESSION['user_link']) and !isset($_GET['user'])){
     function generateRandomString(){
