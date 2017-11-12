e395e54b1d96b186c87a4a3ab25a7eeaa3b230a7
barnettm <barnett717@gmail.com>
2017-11-11 16:15:09 -0800

    revert changes on retunToPageOne function

--- script/main.js
+++ script/main.js
@@ -1054,49 +1054,11 @@ function checkIfPlayer2IsMuted() {
     }
 }
 
-function returnToPageOne(){
-    $(".carousel").fadeOut();
-    $(".carousel").removeClass('slide')
-    $(".carousel").carousel(0)
-    // $(".active").hide();
-        currentSlideNumber = 1; //redundant?
-        var videosToLoad = [];
-        for(var i = 0; i < 40; i++){
-            videosToLoad.push(videoObjectsToLoad[i])
-        }
-        console.log("VIDEOS TO LOAD", videosToLoad);    //load list data while carousel is moving
-        setTimeout(function(){
-            clearVideoList();
-            renderVideoList(videosToLoad)
-        }, 250)
-    // $('.active').hide();
-    $('.carousel').fadeIn("slow")
-    // $(".carousel").fadeIn('slow');
-    
-    $(".carousel").addClass('slide')
-    
-    displayCurrentPageNumber()
-}
-
 // function returnToPageOne(){
-//     $(".active").fadeOut();
-//     if(currentSlideNumber !== 1){
-//         //show backwards animation
-
-//         if(currentSlideNumber % 2 === 0)
-//         {
-//             currentSlideNumber = 2;
-//             $(".carousel").carousel(0);
-            
-//         }
-//         else
-//         {
-//             // currentSlideNumber = 3;
-//             // $(".carousel").carousel('prev');
-//             // $(".carousel").carousel(0);
-//         }
-//         //
-//         //find and load data into list
+//     $(".carousel").fadeOut();
+//     $(".carousel").removeClass('slide')
+//     $(".carousel").carousel(0)
+//     // $(".active").hide();
 //         currentSlideNumber = 1; //redundant?
 //         var videosToLoad = [];
 //         for(var i = 0; i < 40; i++){
@@ -1107,10 +1069,48 @@ function returnToPageOne(){
 //             clearVideoList();
 //             renderVideoList(videosToLoad)
 //         }, 250)
-//     }
-//     $(".active").fadeIn();
-//     displayCurrentPageNumber();
+//     // $('.active').hide();
+//     $('.carousel').fadeIn("slow")
+//     // $(".carousel").fadeIn('slow');
+    
+//     $(".carousel").addClass('slide')
+    
+//     displayCurrentPageNumber()
 // }
+
+function returnToPageOne(){
+    $(".active").fadeOut();
+    if(currentSlideNumber !== 1){
+        //show backwards animation
+
+        if(currentSlideNumber % 2 === 0)
+        {
+            currentSlideNumber = 2;
+            $(".carousel").carousel(0);
+            
+        }
+        else
+        {
+            // currentSlideNumber = 3;
+            // $(".carousel").carousel('prev');
+            // $(".carousel").carousel(0);
+        }
+        //
+        //find and load data into list
+        currentSlideNumber = 1; //redundant?
+        var videosToLoad = [];
+        for(var i = 0; i < 40; i++){
+            videosToLoad.push(videoObjectsToLoad[i])
+        }
+        console.log("VIDEOS TO LOAD", videosToLoad);    //load list data while carousel is moving
+        setTimeout(function(){
+            clearVideoList();
+            renderVideoList(videosToLoad)
+        }, 250)
+    }
+    $(".active").fadeIn();
+    displayCurrentPageNumber();
+}
 
 // check if device is apple mobile device (used to convert date object)
 function checkIfAppleDevice(){
