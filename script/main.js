39d321bf3ac30f1d429eb670f8c1170b18d9cd8d
AnthonySorise <contact@anthonysorise.com>
2017-11-11 19:46:34 -0800

    setTimeout put on initiateUser

--- script/main.js
+++ script/main.js
@@ -85,8 +85,11 @@ $(document).ready(function () {
     setTimeout(()=>{
         iframeRight = $('#mainVideo').position().left + $('#mainVideo').width();
         $('.lightBoxMode').css('left', iframeRight+'px');
-        initiateUser();
     },200);
+
+    setTimeout(()=>{
+        initiateUser();
+    }, 750)
 });
 
 
