b8b6cf39c98db42fee2a00d80d784a447215aaeb
collin_wong <collinwong53@gmail.com>
2017-11-15 17:36:47 -0800

    switching to dev

--- script/api_calls_to_db/access_database/delete_ctu.php
+++ script/api_calls_to_db/access_database/delete_ctu.php
@@ -6,8 +6,8 @@ if(empty($_POST['youtube_channel_id'])){
     $output['errors'] = 'MISSING YOUTUBE CHANNEL ID';
 }
 $youtube_channel_id = $_POST['youtube_channel_id'];
-// $user_link = $_SESSION['user_link'];
-// $user_id = USER_ID;
+$user_link = $_SESSION['user_link'];
+$user_id = USER_ID;
 $stmt = $conn->prepare("DELETE ctu FROM channels_to_users ctu 
 JOIN channels c ON ctu.channel_id = c.channel_id 
 JOIN users u ON ctu.user_id = u.user_id
