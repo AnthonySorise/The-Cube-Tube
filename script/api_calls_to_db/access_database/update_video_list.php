0d561e05f53b6d7434e424212e41d833065a8026
collin_wong <collinwong53@gmail.com>
2017-11-17 10:24:56 -0800

    change read channel array

--- script/api_calls_to_db/access_database/update_video_list.php
+++ script/api_calls_to_db/access_database/update_video_list.php
@@ -4,4 +4,11 @@ if(empty($LOCAL_ACCESS)){
 }
 require_once("youtube_api_key");
 $youtube_channel_id = $_POST['youtube_channel_id'];
+if(empty($youtube_channel_id)){
+    $output['errors'][] = "MISSING CHANNEL ID";
+    output_and_exit($output);
+}
+$last_pull_date = date(("Y-m-d H:i:s"));
+$sqli = "UPDATE channels SET last_published = ? WHERE youtube_channel_id = ?";
+$stmt = 
 ?>
