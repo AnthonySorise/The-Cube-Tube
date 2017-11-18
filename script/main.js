bec8e0a90bedbb892dca6401fbaa74a5d73dd774
AnthonySorise <contact@anthonysorise.com>
2017-11-18 11:17:57 -0800

    channel ajax call fix

--- script/main.js
+++ script/main.js
@@ -566,15 +566,33 @@ function initiateUser() {
                         },
                         success: function (data) {
                             if (data.success) {
-                                console.log('read data success', data.data);
-                                // data.data[0].youtube_channel_id = channelId;
-                                clientSubscribedChannelObjects.push(data.data[0]);
-                                clientSelectedChannelObjects.push(data.data[0]);
+                                $.ajax({
+                                    url:'./script/api_calls_to_db/access_database/access.php',
+                                    method:'post',
+                                    dataType:'JSON',
+                                    data:{
+                                        youtube_channel_id:youtube_channel_id,
+                                        action:'read_channels_by_youtube_id'
+                                    },
+                                    success:function(data){
+                                        if(data.success){
+                                            console.log('Channel read from DB', data.data);
+                                            // data.data[0].youtube_channel_id = channelId;
+                                            clientSubscribedChannelObjects.push(data.data[0]);
+                                            clientSelectedChannelObjects.push(data.data[0]);
 
-                                if (numSubscribedChannels === clientSubscribedChannelObjects.length) {
-                                    loadSelectedChannels();
-                                    renderChannelSelectionDropdown();
-                                }
+                                            if (numSubscribedChannels === clientSubscribedChannelObjects.length) {
+                                                loadSelectedChannels();
+                                                renderChannelSelectionDropdown();
+                                            }
+                                        }else{
+                                            console.log(data);
+                                        }
+                                    },
+                                    errors:function(data){
+                                        console.log(data['errors'], data);
+                                    }
+                                })
                             } else {
                                 console.log(data);
                             }
