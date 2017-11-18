d530362c9eb390e34d3caff21fc2cfc1b72f72f1
AnthonySorise <contact@anthonysorise.com>
2017-11-18 13:41:25 -0800

    read_channels_by_user_id fix

--- script/api_calls_to_db/access_database/read_channels_by_user_id.php
+++ script/api_calls_to_db/access_database/read_channels_by_user_id.php
@@ -6,6 +6,7 @@ if(empty($_SESSION['user_link'])){
     $output['user'] = false;
     output_and_exit($output);
 }
+$user_link = $_SESSION['user_link'];
 $stmt = $conn->prepare("SELECT c.channel_title, 
 c.youtube_channel_id,c.description,c.thumbnail_file_name,c.last_channel_pull
 FROM channels AS c 
@@ -15,7 +16,7 @@ JOIN users AS u
 ON u.user_id = ctu.user_id
 WHERE u.user_link = ?
 ORDER BY c.channel_title");
-$stmt->bind_param('i',$user_link);
+$stmt->bind_param('s',$user_link);
 $stmt->execute();
 $result = mysqli_stmt_get_result($stmt);
 if(!empty($result)){
