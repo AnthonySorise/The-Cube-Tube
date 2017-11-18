858461e4f49c6b59d001ac9d7fef74300a6221c1
collin_wong <collinwong53@gmail.com>
2017-11-17 20:07:13 -0800

    removed exit at video curl

--- script/api_calls_to_db/access_database/youtube_videos_curl.php
+++ script/api_calls_to_db/access_database/youtube_videos_curl.php
@@ -46,7 +46,6 @@ function insert_videos($youtube_channel_id,$channel_id,$page_token,$DEVELOPER_KE
       } else {
             $video_array = json_decode($json, true);
             print_r($video_array);
-            exit();
             $next_page_token = $video_array['nextPageToken'];
             $output['token'] = $next_page_token;
             $entries = $video_array['items'];
