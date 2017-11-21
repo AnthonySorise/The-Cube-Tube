8f11a1d0d14fd0e597da1778b25f85f5141a54b3
Umpoy <iumpoy@yahoo.com>
2017-11-20 17:50:34 -0800

    fixed erros

--- script/main.js
+++ script/main.js
@@ -186,7 +186,7 @@ function tooltipFunctions() {
 function clickHandler() {
     $('.channelDropDown').on('click touchend', '.dropdownChannelLiLoad', () => {
         browsingMode = false;
-        // returnToPageOne();
+        returnToPageOne();
         compileSelectedChannelsFromDropdown();
 
         // var numUpdated = 0;
@@ -224,14 +224,13 @@ function clickHandler() {
         //     })
         //
         // }
-        $('#copyToClipboard').on('click', copy_to_clipboard);
     });
 
     $(".dropdownChannelLiAll").on("click", function () {
         browsingMode = false;
         clientSelectedChannelIds = deepCopy(clientSubscribedChannelIds);
         clientSelectedChannelObjects = deepCopy(clientSubscribedChannelObjects);
-        // returnToPageOne();
+        returnToPageOne();
         renderChannelSelectionDropdown();
         loadSelectedChannels();
         if (window.innerWidth < 500) {
@@ -568,6 +567,9 @@ function clickHandler() {
         }
 
     }
+    //TM87
+    $('.glyphicon-copy').on('click', copy_to_clipboard)
+
 }
 
 function initiateUser() {
@@ -815,7 +817,6 @@ function renderChannelSelectionDropdown() {
         var settingsContent = $('<div>', {
             'channelId': clientSubscribedChannelObjects[i].youtube_channel_id
         });
-        settingsContent.css("background-color", "black");
 
         var browseButton = $('<button class="btn-primary">Browse</button>').css("display", "block");
         var removeButton = $('<button class="btn-danger">Unsubscribe</button>').css("display", "block").css("margin-top", "5px");
@@ -959,7 +960,7 @@ function renderVideoList(videoArray) {
     }
     console.log("LOADING VIDEO LIST")
     clearVideoList();
-    // removePlaceHolderAnimation();
+
     for (let i = 0; i < videoArray.length; i++) {
         if (videoArray[i] === undefined) {
             return
@@ -1026,12 +1027,11 @@ function renderVideoList(videoArray) {
 }
 
 function addChannelModal(userLink) {
-    var user_id_input = `www.TheCubeTube.com/?user=${userLink}`
+    let button = $('<button>')
     if (userLink) {
-        $('.userLinkBody').text("Save this link! ");
-        $('.userLinkBody').append('<span class="glyphicon glyphicon-copy" aria-hidden="true"></span>').attr('id', 'copyToClipboard');
-        $('.userLinkBody').append('<input id="cubetube_user_id">').val(user_id_input).attr('id', 'cubetube_user_id');
-        console.log(user_id_input);
+        $('.userLinkBody').text("Save this link!  www.TheCubeTube.com/?user=" + userLink);
+        $('.userLinkBody').append(button).addClass('glyphicon glyphicon-copy')
+
     }
     else {
         $('.userLinkBody').text("Channel added to your subscriptions!")
@@ -1039,11 +1039,21 @@ function addChannelModal(userLink) {
     $('#userLinkModal').modal('show');
 }
 
-function copy_to_clipboard(){
-    var copy_text = document.getElementById('cubetube_user_id');
-    copy_text.select();
-    document.execCommand('Copy');
+function copy_to_clipboard() {
+    var textArea = document.createElement("textarea");
+    textArea.style.background = 'transparent';
+    textArea.value = $('.userLinkBody').text().slice(17,55);
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
 }
+
 
 // function ytChannelApiToDb(channelId, isAdding = false) {
 //     var channelDbObject = {};
@@ -1356,7 +1366,7 @@ function retrieveInfoFromDB(channelID, isAdding = false) {
                                             console.log('insert error', data);
                                         }
                                     })
-                                }, 2000);
+                                }, 1000);
                             }
                         },
                         errors: function (data) {
@@ -1392,10 +1402,6 @@ function handleBrowseButton() {
 
 function handleAddButton() {
     //CALL FUNCTION THAT LOOKS SELECTION LIST AND UPDATES clientSelectedChannelIds and and clientSelectedChannelObjects
-
-    clearVideoList();
-    // createPlaceholderAnimation();
-
     videoObjectsToLoad = [];
     if (browsingMode) {
         clientSelectedChannelIds = [];
@@ -1404,6 +1410,11 @@ function handleAddButton() {
     }
 
     browsingMode = false;
+
+    returnToPageOne();
+    // clearVideoList();
+    // createPlaceholderAnimation();
+
 
 
     let channelID = $(this).parent().attr("channelId");
@@ -1458,20 +1469,20 @@ function getAutoPlayValue() {
 
 //Testing placeholder animation
 
-// function createPlaceholderAnimation() {
-//     $(".tdList").show();
-//
-//     var outerDiv = $('<div>').addClass("timeline-wrapper");
-//     var nestedDiv1 = $('<div>').addClass("timeline-item");
-//     var nestedDiv2 = $('<div>').addClass("animated-background");
-//     var nestedDiv3 = $('<div>').addClass("background-masker");
-//     var completedWrapper = $(outerDiv).append(nestedDiv1, nestedDiv2, nestedDiv3);
-//     $('.tdTitle, .tdChannel, .tdUpdate').append(completedWrapper);
-// }
-//
-// function removePlaceHolderAnimation() {
-//     $('.timeline-wrapper').remove()
-// }
+function createPlaceholderAnimation() {
+    $(".tdList").show();
+
+    var outerDiv = $('<div>').addClass("timeline-wrapper");
+    var nestedDiv1 = $('<div>').addClass("timeline-item");
+    var nestedDiv2 = $('<div>').addClass("animated-background");
+    var nestedDiv3 = $('<div>').addClass("background-masker");
+    var completedWrapper = $(outerDiv).append(nestedDiv1, nestedDiv2, nestedDiv3);
+    $('.tdTitle, .tdChannel, .tdUpdate').append(completedWrapper);
+}
+
+function removePlaceHolderAnimation() {
+    $('.timeline-wrapper').remove()
+}
 
 
 function removeUnusedRows() {
