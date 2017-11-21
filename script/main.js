98603ce8762a17a535265432381abeb60e2b3b18
AnthonySorise <contact@anthonysorise.com>
2017-11-20 19:10:41 -0800

    fix

--- script/main.js
+++ script/main.js
@@ -1356,33 +1356,34 @@ function retrieveInfoFromDB(channelID, isAdding = false) {
                                         console.log("ERROR", data);
                                     }
                                 })
+                                //update remaining videos
+                                setTimeout(function(){
+                                    $.ajax({
+                                        url:'./script/api_calls_to_db/access_database/access.php',
+                                        method:'post',
+                                        dataType:'JSON',
+                                        data:{
+                                            action:'insert_videos_curl',
+                                            youtube_channel_id:channelID,
+                                            page_token:data.page_token
+                                        },
+                                        success: function (data) {
+                                            if (data.success) {
+                                                console.log('All videos inserted to DB from YouTube', data);
+                                            }
+                                        },
+                                        errors: function (data) {
+                                            console.log('insert error', data);
+                                        }
+                                    })
+                                }, 2000);
+
                             }
                         },
                         errors: function (data) {
                             console.log("ERROR", data);
                         }
                     })
-                    //update remaining videos
-                    setTimeout(function(){
-                        $.ajax({
-                            url:'./script/api_calls_to_db/access_database/access.php',
-                            method:'post',
-                            dataType:'JSON',
-                            data:{
-                                action:'insert_videos_curl',
-                                youtube_channel_id:channelID,
-                                page_token:data.page_token
-                            },
-                            success: function (data) {
-                                if (data.success) {
-                                    console.log('All videos inserted to DB from YouTube', data);
-                                }
-                            },
-                            errors: function (data) {
-                                console.log('insert error', data);
-                            }
-                        })
-                    }, 2000);
                 }
             }
         },
