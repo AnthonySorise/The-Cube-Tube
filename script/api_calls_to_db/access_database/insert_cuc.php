70b64dd2be5d3a88d0ccabaf7193abb938b527f2
collin_wong <collinwong53@gmail.com>
2017-11-20 20:36:52 -0800

    finished insert cuc need to test

--- script/api_calls_to_db/access_database/insert_cuc.php
+++ script/api_calls_to_db/access_database/insert_cuc.php
@@ -3,6 +3,21 @@ if(empty($LOCAL_ACCESS)){
     die('insert ctu, direct access not allowed');
 }
 $user_id = USER_LINK;
+//grabbing channel id
+$sqli = "SELECT channel_id
+FROM channels WHERE youtube_channel_id = ?";
+$stmt = $conn->prepare($sqli);
+$stmt->bind_param('s', $youtube_channel_id);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_row>0) {
+    $row = $result->fetch_assoc();
+    $channel_id = $row['channel_id'];
+} else {
+    $output['messages'] = "can't read channel";
+    output_and_exit($output);
+}
+//inserting the link between user channel and category
 $sqli = "INSERT INTO category_to_user_to_channel 
 SET user_id = ?, channel_id = ?, category_id = ?";
 $stmt = $conn->prepare($sqli);
@@ -12,10 +27,11 @@ if(empty($stmt)){
     $output['errors'][]= 'invalid query';
     output_and_exit($output);
 }else{
-    if(mysqli_affected_rows($conn)>0){
+    if($conn->affected_rows>0){
         $output['success'] = true;
     }else{
         $output['errors'][] = "could not insert cuc";
+        output_and_exit($output);
     }
 }
 
