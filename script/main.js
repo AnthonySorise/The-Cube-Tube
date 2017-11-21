c29bc3b79b7a6a1c6acd5c2a2a92b30380485183
AnthonySorise <contact@anthonysorise.com>
2017-11-20 16:52:48 -0800

    update channel ajax call uncommented

--- script/main.js
+++ script/main.js
@@ -1232,25 +1232,26 @@ function retrieveInfoFromDB(channelID, isAdding = false) {
                 console.log('Channel Found on DB', data);
                 console.log("last channel pull: ", data.data[0].last_channel_pull)
                 //update Channel
-                // $.ajax({
-                //     url:'./script/api_calls_to_db/access_database/access.php',
-                //     method:'post',
-                //     dataType:'JSON',
-                //     data:{
-                //         action:'update_video_list',
-                //         youtube_channel_id:channelID,
-                //         last_channel_pull:data.data[0].last_channel_pull
-                //     },
-                //     success: function (data) {
-                //         if (data.success) {
-                //             console.log('Channel Updated', data);
-                            handleInfoFromDB(data);
-                //         }
-                //     },
-                //     errors: function (data) {
-                //         console.log('insert error', data);
-                //     }
-                // })
+                $.ajax({
+                    url:'./script/api_calls_to_db/access_database/access.php',
+                    method:'post',
+                    dataType:'JSON',
+                    data:{
+                        action:'update_video_list',
+                        youtube_channel_id:channelID,
+                        last_channel_pull:data.data[0].last_channel_pull
+                    },
+                    success: function (data) {
+                        if (data.success) {
+                            console.log('Channel Updated', data);
+                        }
+                        console.log(data);
+                        handleInfoFromDB(data);
+                    },
+                    errors: function (data) {
+                        console.log('insert error', data);
+                    }
+                })
             }
             //Channel NOT on DB
             else{
