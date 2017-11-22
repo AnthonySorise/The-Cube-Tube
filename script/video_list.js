29cd85af04454d91be2b810d38c5a304b3b8af14
AnthonySorise <contact@anthonysorise.com>
2017-11-21 19:57:50 -0800

    info popover width

--- script/video_list.js
+++ script/video_list.js
@@ -343,7 +343,7 @@ function updateVideoInfoPopover(videoID){
 
             const descriptionContainer = $('<div></div>');
             descriptionContainer.css("height", "13vh");
-            descriptionContainer.css("width", "20vw");
+            descriptionContainer.css("width", "35vw");
             descriptionContainer.css("overflow-y", "auto")
             const description = $('<p>' + data.items[0].snippet.description + '</p>');
             descriptionContainer.append(description);
@@ -394,7 +394,7 @@ function updateChannelInfoPopover(channelID){
             const descriptionTitle = $('<p><strong>Description: </strong></p>');
             const descriptionContainer = $('<div></div>');
             descriptionContainer.css("height", "21.75vh");
-            descriptionContainer.css("width", "20vw");
+            descriptionContainer.css("width", "35vw");
             descriptionContainer.css("overflow-y", "auto")
             const description = $('<p>' + data.items[0].snippet.description + '</p>');
             descriptionContainer.append(description);
