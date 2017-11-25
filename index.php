1d55f881b59103464f636762db399f4940fc6744
briandhkim <w1057216@gmail.com>
2017-11-25 13:30:33 -0800

    button wrapped in div

--- index.php
+++ index.php
@@ -1685,11 +1685,13 @@ consider carousel for the video list area:
 								<h5 class="visible-xs">Categorize your channel:</h5>
 							<div class="userCategoryExists">
 								<label>Add to existing categories</label>
-								<select name="initialCategorySelect" class="channelCategorySelect form-control" style="width:60%; margin-left:20%; margin-bottom:20px;">
-									<option value="cat1">cat1</option>
-									<option value="cat2">cat2</option>
-								</select>
-								<button class="btn btn-success pull-right">add</button>
+								<div>
+									<select name="initialCategorySelect" class="channelCategorySelect form-control" style="width:60%; margin-left:20%; margin-bottom:20px;">
+										<option value="cat1">cat1</option>
+										<option value="cat2">cat2</option>
+									</select>
+									<button class="btn btn-success pull-right">add</button>
+								</div>
 								<dl class="col-xs-12 text-center" style="margin-bottom:20px;">
 									<dd> ------or------ </dd>
 								</dl>
