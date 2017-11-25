8814070d041e148d2fb3f218b67dd3f2d2607fe0
AnthonySorise <contact@anthonysorise.com>
2017-11-25 13:29:24 -0800

    planning out category front-end

--- script/categories.js
+++ script/categories.js
@@ -1,5 +1,14 @@
 var clientCategories = {};
 
 function handleChangeCategory(){
+    //opens the change category modal
+}
+
+function changeCategory(category){
+    //ajax calls to remove category
+
+    //ajax calls to insert category
+
+    //front end changes to clientCategories and call renderChannelSelectionDropdown()
 
 }
