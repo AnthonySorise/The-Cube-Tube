9acae6fd3a42fec6e819ac3fd450412e580a3444
collin_wong <collinwong53@gmail.com>
2017-11-25 17:03:56 -0800

    fix name in delete cuc

--- script/api_calls_to_db/access_database/delete_cuc.php
+++ script/api_calls_to_db/access_database/delete_cuc.php
@@ -8,7 +8,7 @@
         "DELETE
             cuc
         FROM 
-            categories_to_users_to_channels AS cuc
+            category_to_user_to_channel AS cuc
         JOIN 
             users AS u ON u.user_id = cuc.user_id
         JOIN 
@@ -16,15 +16,14 @@
         WHERE 
             u.user_link = ? AND c.youtube_channel_id = ?";
     $stmt = $conn->prepare($sqli);
-    $stmt->bind_param('ss',$user_link,$youtube_channel_id);
-    $stmt->execute();
-    if(empty($stmt)){
+    if(!$stmt->bind_param('ss',$user_link,$youtube_channel_id)){
         $output['errors'][] = 'delete cuc statement failed after delete ctu';
+    };
+    $stmt->execute();
+    if($conn->affected_rows>0){
+        $output['messages'][] = 'deleted cucs';
     }else{
-        if($conn->affected_rows>0){
-            $output['messages'][] = 'deleted cucs';
-        }else{
-            $output['messages'][] = 'no cucs to remove';
-        }
+        $output['messages'][] = 'no cucs to remove';
     }
+    
 ?>
