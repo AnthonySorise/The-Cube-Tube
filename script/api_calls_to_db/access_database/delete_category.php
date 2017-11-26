6373d37db5c724bd8740029202b5d14655b6e8e7
collin_wong <collinwong53@gmail.com>
2017-11-25 22:37:55 -0800

    testing query delete cat

--- script/api_calls_to_db/access_database/delete_category.php
+++ script/api_calls_to_db/access_database/delete_category.php
@@ -15,22 +15,21 @@ $sqli =
     FROM
         categories AS ct
     JOIN
-        categories_to_users_to_channels AS cuc ON cuc.category_id = ct.category_id
+        category_to_user_to_channel AS cuc ON cuc.category_id = ct.category_id
     JOIN
         users AS u ON u.user_id = cuc.user_id
     WHERE
         ct.category_name = ? AND u.user_link = ?";
-$stmt = $conn->prepare($sqli);
-$stmt->bind_param('ss',$category_name,$user_link);
-if(!$stmt->execute()){
-    $output['errors'] = 'statement failed';
+if(!($stmt = $conn->prepare($sqli))){
+    $output['errors'][] = 'delete category statement failed';
     output_and_exit($output);
+}
+$stmt->bind_param('ss',$category_name,$user_link);
+$stmt->execute();
+if($conn->affected_rows>0){
+    $output['success'] = true;
+    $output['messages'][] = 'delete successful';
 }else{
-    if($conn->affected_rows>0){
-        $output['success'] = true;
-        $output['messages'][] = 'delete successful';
-    }else{
-        $output['errors'][] = 'nothing to delete';
-    }
+    $output['errors'][] = 'nothing to delete';
 }
 ?>
