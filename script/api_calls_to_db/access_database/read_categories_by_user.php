8814f97092c6acdfc2340f8bd80c50c11cea1611
collin_wong <collinwong53@gmail.com>
2017-11-26 02:33:10 -0800

    fix typo in read cat

--- script/api_calls_to_db/access_database/read_categories_by_user.php
+++ script/api_calls_to_db/access_database/read_categories_by_user.php
@@ -6,7 +6,7 @@ if(empty($_SESSION['user_link'])){
     $output['messages'] = 'no user';
     output_and_exit($output);
 }
-$stmt = $conn->prepare(
+$query = 
     "SELECT
         c.channel_title,
         c.youtube_channel_id,
@@ -14,7 +14,7 @@ $stmt = $conn->prepare(
     FROM
         channels AS c
     JOIN
-        category_to_users_to_channel AS cuc ON c.channel_id = cuc.channel_id
+        category_to_user_to_channel AS cuc ON c.channel_id = cuc.channel_id
     JOIN
         users AS u ON u.user_id = cuc.user_id
     JOIN
@@ -22,7 +22,11 @@ $stmt = $conn->prepare(
     WHERE
         u.user_link = ?
     ORDER BY
-        ct.category_name");
+        ct.category_name";
+if(!($stmt = $conn->prepare($query))){
+    $output['errors'][] = 'invalid query at read categories';
+    output_and_exit($output);
+};
 $stmt->bind_param('s',$user_link);
 $stmt->execute();
 $result = $stmt->get_result();
