74dca94f92311e87ad64ae59332f938b7b0abf5b
collinwong53 <collinwong53@gmail.com>
2017-11-27 23:42:41 -0800

    reverted some changes for update channel

--- script/api_calls_to_db/access_database/update_channels.php
+++ script/api_calls_to_db/access_database/update_channels.php
@@ -1,18 +1,8 @@
 <?php
-// if(empty($LOCAL_ACCESS)){
-//     die('direct access not allowed');
-// }
-require_once('mysql_connect.php');
+ if(empty($LOCAL_ACCESS)){
+     die('direct access not allowed');
+ }
 require_once('youtube_api_key.php');
-$output = [
-    'success' => false,
-    'errors' => [],
-];
-function output_and_exit($output){
-    $json_output = json_encode($output);
-    print($json_output);
-    exit();
-}
 $query = 
     "SELECT 
         youtube_channel_id 
