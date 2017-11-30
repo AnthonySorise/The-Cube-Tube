28230f1a72860b3b265fa5f5eab08f5324c7f9b5
AnthonySorise <contact@anthonysorise.com>
2017-11-29 22:40:54 -0800

    info popover description size

--- script/video_list.js
+++ script/video_list.js
@@ -386,7 +386,7 @@ function updateVideoInfoPopover(videoID){
             const descriptionTitle = $('<p><strong>Description: </strong></p>');
 
             const descriptionContainer = $('<div></div>');
-            descriptionContainer.css("height", "40%");
+            descriptionContainer.css("height", "33%");
             descriptionContainer.css("overflow-y", "auto")
             const description = $('<p>' + data.items[0].snippet.description + '</p>');
             descriptionContainer.append(description);
@@ -436,7 +436,7 @@ function updateChannelInfoPopover(channelID){
             var subscriberCount = $('<p><strong>Subscribers: </strong>' + parseInt(data.items[0].statistics.subscriberCount).toLocaleString("en-us") + '</p>');
             const descriptionTitle = $('<p><strong>Description: </strong></p>');
             const descriptionContainer = $('<div></div>');
-            descriptionContainer.css("height", "40%");
+            descriptionContainer.css("height", "33%");
 
             descriptionContainer.css("overflow-y", "auto")
             const description = $('<p>' + data.items[0].snippet.description + '</p>');
