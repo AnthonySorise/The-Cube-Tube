2fdb6f452c5315c2692361a35898f8b482660d40
collin_wong <collinwong53@gmail.com>
2017-11-30 15:00:56 -0800

    added some comments and text reference file for apis

--- script/api_calls_to_db/access_database/youtube_videos_curl.php
+++ script/api_calls_to_db/access_database/youtube_videos_curl.php
@@ -7,7 +7,7 @@ if(!empty($_POST['page_token'])){
     $next_page_token = $_POST['page_token'];
     $youtube_channel_id = $_POST['youtube_channel_id'];
 }
-if(!empty($_POST['last_channel_pull'])){
+if(!empty($_POST['last_channel_pull'])){//change datetime to youtube standard format
     $last_channel_pull = $_POST['last_channel_pull'];
     $last_channel_pull = str_replace(' ','T', $last_channel_pull);
     $last_channel_pull .= '.000Z';
