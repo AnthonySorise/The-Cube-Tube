ee83bc08bad5613275a9d89b640ef8e7a4192f3a
collin_wong <collinwong53@gmail.com>
2017-12-13 14:30:12 -0800

    removed dns from show query

--- script/api_calls_to_db/access_database/show_user_query.php
+++ script/api_calls_to_db/access_database/show_user_query.php
@@ -2,5 +2,6 @@
 if(empty($LOCAL_ACCESS)){
     die('no direct access allowed');
 }
-header("Location:www.dev.thecubetube.com/?user={$_SESSION['user_link']}");
+header("Location:/?user={$_SESSION['user_link']}");
+exit();
 ?>
