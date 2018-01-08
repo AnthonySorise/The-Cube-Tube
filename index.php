7fe02f391bb3c9e5b6afaf463fb76fa1f8092054
AnthonySorise <contact@anthonysorise.com>
2018-01-08 13:26:43 -0800

    added 'the' to title

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ consider carousel for the video list area:
 	<script type="text/javascript" src="library/velocity.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
 	<link rel="icon" type='image/png' href="assets/images/ctube_logo.png" sizes="32x32">
-	<title>CubeTube</title>
+	<title>TheCubeTube</title>
 </head>
 
 <body>
