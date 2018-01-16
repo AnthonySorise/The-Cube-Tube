c56c4aa35d539a33fea1bdb2e4b592cd90956d2d
collinwong53 <collinwong53@gmail.com>
2018-01-16 00:49:56 -0800

    test no filter on title

--- script/api_calls_to_db/access_database/videos/youtube_videos_curl.php
+++ script/api_calls_to_db/access_database/videos/youtube_videos_curl.php
@@ -103,11 +103,11 @@ function insert_videos($youtube_channel_id,$channel_id,$page_token,$DEVELOPER_KE
                 $query .= " (?,?,?,?,?),";
                 $bind_str .= "sisss";
                 //grab relavent data from youtube and put it into an array
-                $data[] = filter_var($value['snippet']['title'], FILTER_SANITIZE_STRING);//youtube video title
+                // $data[] = filter_var($value['snippet']['title'], FILTER_SANITIZE_STRING);//youtube video title
+                $datap[] = $value['snippet']['title'];
                 $data[] = $channel_id;
                 $data[] = filter_var($value['id']['videoId'], FILTER_SANITIZE_STRING);//youtube video id
-                // $data[] = filter_var($value['snippet']['description'], FILTER_SANITIZE_STRING);//description
-                $data[] = $value['snippet']['description'];
+                $data[] = filter_var($value['snippet']['description'], FILTER_SANITIZE_STRING);//description
                 //break datetime given from youtube to datetime that can be entered into database
                 $published_at = $value['snippet']['publishedAt'];
                 $published_at = str_replace('T',' ',$published_at);
