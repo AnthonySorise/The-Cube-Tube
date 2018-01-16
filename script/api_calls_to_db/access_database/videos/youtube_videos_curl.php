af9eeef42c831690fbcc3ffe374cd4d65d41cfe1
collinwong53 <collinwong53@gmail.com>
2018-01-16 00:45:32 -0800

    test no filter on date

--- script/api_calls_to_db/access_database/videos/youtube_videos_curl.php
+++ script/api_calls_to_db/access_database/videos/youtube_videos_curl.php
@@ -105,16 +105,16 @@ function insert_videos($youtube_channel_id,$channel_id,$page_token,$DEVELOPER_KE
                 //grab relavent data from youtube and put it into an array
                 $data[] = filter_var($value['snippet']['title'], FILTER_SANITIZE_STRING);//youtube video title
                 $data[] = $channel_id;
-                // $data[] = filter_var($value['id']['videoId'], FILTER_SANITIZE_STRING);//youtube video id
-                $data[] = $value['id']['videoId'];
+                $data[] = filter_var($value['id']['videoId'], FILTER_SANITIZE_STRING);//youtube video id
                 $data[] = filter_var($value['snippet']['description'], FILTER_SANITIZE_STRING);//description
                 //break datetime given from youtube to datetime that can be entered into database
                 $published_at = $value['snippet']['publishedAt'];
                 $published_at = str_replace('T',' ',$published_at);
                 $published_at = str_replace('.000Z','',$published_at);
-                if(validateDate($published_at)){
-                    $data[] = $published_at;
-                };
+                // if(validateDate($published_at)){
+                //     $data[] = $published_at;
+                // };
+                $data[] = $published_at;
             }
         }
         //remove last comma
